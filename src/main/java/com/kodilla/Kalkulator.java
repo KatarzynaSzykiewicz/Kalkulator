644b5469d62850c10e1f56a60b80e9532e74c6e5
KatarzynaSzykiewicz <szykiewicz_katarzyn a@wp.pl>
2017-12-28 17:41:31 +0100

    first commit

---
+++ src/main/java/com/kodilla/Kalkulator.java
@@ -0,0 +1,14 @@
+package com.kodilla;
+
+class Calculator{
+
+    private void someMethod(){
+
+    }
+	public static void main (String[] args){
+        Calculator calculator = new Calculator();
+        calculator.someMethod();
+
+        System.out.println("It works");
+}
+}
