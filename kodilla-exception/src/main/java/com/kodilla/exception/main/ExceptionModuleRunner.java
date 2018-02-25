2c775495fc1512030fa0830853991adc380f472c
KatarzynaSzykiewicz <szykiewicz_katarzyn a@wp.pl>
2018-02-25 21:07:56 +0100

    Submodule 8.3

--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -1,20 +1,16 @@
 package com.kodilla.exception.main;
 
-import com.kodilla.exception.io.FileReaderWithoutHandling;
-
-import java.io.IOException;
+import com.kodilla.exception.io.FileReader;
+import com.kodilla.exception.io.FileReaderException;
 
 public class ExceptionModuleRunner {
     public  static  void main(String args[]){
 
-//        FileReader fileReader = new FileReader();
-//        fileReader.readFile();
-
-//        FileReaderWithoutHandling fileReaderWithoutHandling = new FileReaderWithoutHandling();
-//        try {
-//            fileReaderWithoutHandling.readFile();
-//        } catch (IOException e) {
-//            e.printStackTrace();
+        FileReader fileReader = new FileReader();
+        try {
+            fileReader.readFile();
+        } catch (FileReaderException e) {
+            System.out.println("Problem while reading a file!");
         }
     }
 }
