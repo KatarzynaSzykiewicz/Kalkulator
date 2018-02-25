885ebc337f290d13b1824be39a38d6d8e87a96ec
KatarzynaSzykiewicz <szykiewicz_katarzyn a@wp.pl>
2018-02-25 21:11:23 +0100

    Zad 8.2

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -1,9 +1,5 @@
 package com.kodilla.exception.test;
 
-import com.sun.org.apache.xpath.internal.SourceTree;
-
-import java.io.IOException;
-
 public class FirstChallenge {
     public double divide(double a, double b) throws ArithmeticException {
         if (b == 0) {
@@ -22,9 +18,10 @@ public class FirstChallenge {
 
         FirstChallenge firstChallenge = new FirstChallenge();
 
-        try (double result = firstChallenge.divide(3, 0)) {
+        try {
+            double result = firstChallenge.divide(3, 0);
             System.out.println(result);
-        } catch (IOException e) {
+        } catch (ArithmeticException e) {
             System.out.println("Sth went wrong: " + e);
         } finally {
             System.out.println("Finally.");
