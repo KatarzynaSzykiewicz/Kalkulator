045bf58ddc90597b550e07a05067eb78b4d70731
Maciej Rudnicki <maciejrudnicki11@gmail.com>
2018-03-21 17:26:48 +0100

    proposition of task

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/flightFinder/FlightFinder.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/flightFinder/FlightFinder.java
@@ -6,10 +6,25 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class FlightFinder {
-    ArrayList<Flight> destinations = new ArrayList<>();
+    List<Flight> destinations = new ArrayList<>();
     private String cityTo;
     private String cityFrom;
     private LocalDate dateOfDeparture;
+
+    public static void main(String[] args) {
+
+        Flight flight1 = new Flight("GDANSK", "KRAKOW", LocalDate.now());
+        Flight flight2 = new Flight("GDANSK", "WARSZAWA", LocalDate.now());
+        Flight flight3 = new Flight("KRAKOW", "WROCLAW", LocalDate.now());
+
+        List<Flight> flights = new ArrayList<>();
+        flights.add(flight1);
+        flights.add(flight2);
+        flights.add(flight3);
+
+        new FlightFinder(flights, "WROCLAW", "GDANSK", LocalDate.now()).findTheFlight();
+
+    }
 
 
     public void findTheFlight() {
@@ -29,21 +44,36 @@ public class FlightFinder {
         System.out.println("Direct flights to " + cityTo + ", on the " + dateOfDeparture + " are from " + allFlightsToACity);
 
 
-        List<Flight> common = new ArrayList<>(allFlightsFromACity);
-        common.retainAll(allFlightsToACity);
+        List<String> citiesInTheMiddle = allFlightsFromACity.stream()
+                .map(flight -> flight.getCityOfArrival())
+                .collect(Collectors.toList());
 
-        System.out.println("You can reach " + cityTo + ", from " + cityFrom + ", on " + dateOfDeparture + ", flying through: " + common);
+        List<Flight> secondFlight = allFlightsToACity.stream()
+                .filter(flight -> citiesInTheMiddle.contains(flight.getCityOfDeparture()))
+                .collect(Collectors.toList());
+
+        List<String> secondFlightDeparturesCities = secondFlight.stream().map(flight -> flight.getCityOfDeparture()).collect(Collectors.toList());
+
+        List<Flight> firstFlight = allFlightsFromACity.stream()
+                .filter(flight -> secondFlightDeparturesCities.contains(flight.getCityOfArrival()))
+                .collect(Collectors.toList());
+
+        // firstFlight + secondFlight
+
+
+        System.out.println("You can reach " + cityTo + ", from " + cityFrom + ", on " + dateOfDeparture + ", flying " +
+                "using flight: " +  firstFlight.get(0) + " "+secondFlight.get(0));
     }
 
 
-    public FlightFinder(ArrayList<Flight> destinations, String cityTo, String cityFrom, LocalDate dateOfDeparture) {
+    public FlightFinder(List<Flight> destinations, String cityTo, String cityFrom, LocalDate dateOfDeparture) {
         this.destinations = destinations;
         this.cityTo = cityTo;
         this.cityFrom = cityFrom;
         this.dateOfDeparture = dateOfDeparture;
     }
 
-    public ArrayList<Flight> getDestinations() {
+    public List<Flight> getDestinations() {
         return destinations;
     }
 
