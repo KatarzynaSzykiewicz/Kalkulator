e661c45f7b8684c63cce146fec7fef95f6719269
KatarzynaSzykiewicz <szykiewicz_katarzyn a@wp.pl>
2018-03-30 20:25:48 +0200

    Rozdział 10.3

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/Board.java
@@ -0,0 +1,9 @@
+package com.kodilla.spring.portfolio;
+
+import java.util.List;
+
+public class Board {
+    public List<String> toDoList;
+    public List<String> inProgressList;
+    public List<String> doneList;
+}
