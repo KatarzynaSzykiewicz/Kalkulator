1b56b6dc79ad0a9b991f6c6b193f06e6dbfe8306
KatarzynaSzykiewicz <szykiewicz_katarzyn a@wp.pl>
2018-03-31 21:27:17 +0200

    Zad. 10.2 Ready

--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
@@ -1,15 +1,12 @@
 package com.kodilla.spring.calculator;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
 public final class Calculator {
 
-@Autowired
+    @Autowired
     private Display display;
 
     public double add(double a, double b) {
--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -1,12 +1,31 @@
 package com.kodilla.spring.calculator;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@SpringBootTest
+@RunWith(SpringRunner.class)
 public class CalculatorTestSuite {
+
+    @Autowired
+    Calculator calculator;
+
     @Test
     public void testCalculations() {
         //Given
         //When
+        double addResult = calculator.add(3.0, 5.0);
+        double subResult = calculator.sub(15.0, 3.0);
+        double divResult = calculator.div(64.0, 8.0);
+        double mulResult = calculator.mul(4.0, 8.0);
         //Then
+        Assert.assertEquals(addResult, 8.0, 0.01);
+        Assert.assertEquals(subResult, 12.0, 0.01);
+        Assert.assertEquals(divResult, 8.0, 0.01);
+        Assert.assertEquals(mulResult, 32.0, 0.01);
     }
 }
